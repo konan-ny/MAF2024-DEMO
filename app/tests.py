60d8662569345f81554fa68d274d76ec5de94497
nykonan39 <nykonan39@gmail.com>
2024-11-26 08:42:29 +0000

    [Add] MAF2024-DEMO

---
+++ app/tests.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+from MAF2024.TestModule import test
+from MAF2024.ConCSE.ConCSE import ConCSE
+
+print(ConCSE.main(base_model="mbert_uncased"))
+# Create your tests here.
